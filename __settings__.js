47eee8949c95744b0b1b2ffb045b32673f862294
Javi-stud <Singcojayvince30@gmail.com>
2025-10-08 12:05:59 +0800

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2329193.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 254085373, 254085374, 254085372 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/254085391/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/254085395/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/254085386/1/ammo.js', 'preload' : true},
+];
